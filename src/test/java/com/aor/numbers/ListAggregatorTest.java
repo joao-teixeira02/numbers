9c7d52e18921d6afbc05459b01c75d7e5cf2ae5f
JoAPSo Pedro Reis Teixeira <up202005437@fe.up.pt>
2021-11-19 16:45:13 +0000

    Step 3

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -1,17 +1,27 @@
 package com.aor.numbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListAggregatorTest {
+
+    private List<Integer> list;
+    private ListAggregator aggregator;
+
+    @BeforeEach
+    public void helper() {
+        list = Arrays.asList(1,2,4,2,5);
+        aggregator = new ListAggregator();
+    }
+
+
     @Test
     public void sum() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
 
-        ListAggregator aggregator = new ListAggregator();
         int sum = aggregator.sum(list);
 
         Assertions.assertEquals(14, sum);
@@ -19,9 +29,7 @@ public class ListAggregatorTest {
 
     @Test
     public void max() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
 
-        ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(list);
 
         Assertions.assertEquals(5, max);
@@ -29,9 +37,7 @@ public class ListAggregatorTest {
 
     @Test
     public void min() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
 
-        ListAggregator aggregator = new ListAggregator();
         int min = aggregator.min(list);
 
         Assertions.assertEquals(1, min);
@@ -39,9 +45,7 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
 
-        ListAggregator aggregator = new ListAggregator();
         int distinct = aggregator.distinct(list);
 
         Assertions.assertEquals(4, distinct);
